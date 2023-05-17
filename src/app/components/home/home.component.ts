01d9eb1f647a8dc380bee44c7e1b541daa0d809f
Flavio Cirillo <scatto2003@gmail.com>
2023-05-18 00:25:44 +0200

    build_portfolio

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HotObservable } from 'rxjs/internal/testing/HotObservable';
 
 @Component({
     selector: 'app-home',
@@ -15,16 +16,24 @@ export class HomeComponent implements OnInit {
         let minute: number = date.getMinutes();
         let second: number = date.getSeconds();
 
+        const timeString = date.toLocaleTimeString('it-IT', { hour: '2-digit', minute: '2-digit' });
+
         const months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         let month: number = date.getMonth() + 1;
         let day: number = date.getDate();
         const days: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-        if (minute < 10) {
-            document.getElementById("hour")!.innerHTML = `${hour}:0${minute}`;
-        } else {
-            document.getElementById("hour")!.innerHTML = `${hour}:${minute}`;
-        }
+        document.getElementById("hour")!.innerHTML = timeString;
+
+        // ----- Invece di fare tutti questi if basta usare la variabile creata sopra "timeString" ------
+        // if (hour === 0) {
+        //     document.getElementById("hour")!.innerHTML = `00:${minute}`;
+        // } else if (minute < 10) {
+        //     document.getElementById("hour")!.innerHTML = `${hour}:0${minute}`;
+        // } else {
+        //     document.getElementById("hour")!.innerHTML = `${hour}:${minute}`;
+        // }
+
         document.getElementById("data")!.innerHTML = `${days[date.getDay()]}, ${months[date.getMonth()]} ${day}`;
     }
 
